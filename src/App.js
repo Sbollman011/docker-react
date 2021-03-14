785fb8ca1119a2f92cb1a2c2a5a09fe80207569c
Sbollman011 <58572531+Sbollman011@users.noreply.github.com>
2021-03-14 15:38:50 -0700

    changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there! How are you???
+          I was changed on the Dev Branch.
         </p>
         <a
           className="App-link"
